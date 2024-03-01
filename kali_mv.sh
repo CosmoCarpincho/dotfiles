82454810cf11a6b329a57df452d976151ec7632b
Cosmo Carpincho <cosmocarpincho@gmail.com>
2024-03-01 09:28:36 -0300

    fuentes iconos

--- kali_mv.sh
+++ kali_mv.sh
@@ -3,23 +3,38 @@
 route_user="$HOME"
 route_bashrc="$route_user/.bashrc"
 route_zshrc="$route_user/.zshrc"
+route_fonts="/usr/share/fonts"
+cosmo_fonts="$route_fonts/cosmo_fonts"
 
 # update y upgrade
-sudo apt update && sudo apt upgrade
-#batcat
+sudo apt update -y && sudo apt upgrade -y
 
-sudo apt install bat
-alias "cat=$(which batcat)"
+
+# Icons
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/NerdFontsSymbolsOnly.zip
+mkdir temp_cosmo
+mv NerdFontsSymbolsOnly.zip temp_cosmo
+unzip ./temp_comso/NerdFontsSymbolsOnly.zip
+mv ./temp_cosmo/* $cosmo_fonts
+
+# Mis config
 echo -e "\n#Mis configs:\n" >>$route_zshrc
+alias 'g=https://github.com'
+echo 'alias g=https://github.com' >>$route_zshrc
+
+if [ ! -d $cosmo_fonts ]; then
+    mkdir -p $cosmo_fonts
+fi
+
+#batcat
+sudo apt install bat -y
 echo "alias cat=$(which batcat)" >>$route_zshrc
+alias "cat=$(which batcat)"
 
 #lsd
-
-sudo apt install lsd
+sudo apt install lsd -y
+echo "alias ls=lsd" >>$route_zshrc
 alias ls='lsd'
 
-echo "alias ls=lsd" >> $route_zshrc
-
 # lvim
-
 source install_lunar_vim.sh
